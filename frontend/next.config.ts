1a30bed9a353016436e9f6ffe8e8a86ac01fa62b
haman211 <Ahmedhaman2001@gmail.com>
2025-06-09 23:48:53 +0300

    sd

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  
 };
 
-export default withBundleAnalyzer(nextConfig);
+export default nextConfig;
